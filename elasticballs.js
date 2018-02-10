bd1e390556a6fc78f943084ff221bd168dea9cd4
Jefferey Cave <jefferey.cave@gmail.com>
2018-02-10 17:45:22 +0000

    Fixing some bounds checking on the balls

--- elasticballs.js
+++ elasticballs.js
@@ -35,19 +35,35 @@ function ElasticBalls(){
 		timer = setInterval(animate, 20);
 	};
 	
+	/**
+	 * Stop the animation associated with the visualization
+	 * 
+	 * The user has indicated they would like the visualization to stop. 
+	 * Since this is an animation that follows the mouse, we have no 
+	 * idea where it is going to stop. Removes it from the screen.
+	 * 
+	 */
 	this.stop = function(){
 		if(timer === null) return;
 		clearInterval(timer);
 		timer = null;
-		dots.forEach(function(d){
-			d.elem.classList.add('fade');
-			setTimeout(function(e){
-				e.remove();
-			},1000,d.elem);
+		// I don't like the fade out effect. I would prefer that 
+		// the elements loose their attachment to the mouse and naturally
+		// fall off the page. It plays with the physics more.
+		dots.forEach(function(d,i){
+			setTimeout(function(){
+				d.elem.style.opacity = 0;
+				setTimeout(d.elem.remove,1000);
+			},i*50);
 		});
 		
 	};
 	
+	
+	/**
+	 * 
+	 * 
+	 */
 	this.isActive = function(){
 		return timer !== null;
 	};
@@ -69,6 +85,7 @@ function ElasticBalls(){
 		});
 	}
 	
+	
 	/**
 	 * 
 	 */
@@ -81,7 +98,9 @@ function ElasticBalls(){
 				'width:0.5em',
 				'height:0.5em',
 				'border-radius:0.25em',
-				'position:absolute'
+				'position:absolute',
+				'opacity:1',
+				'transition:opacity 1s'
 			].join(';');
 		
 		this.X = Xpos;
@@ -92,87 +111,110 @@ function ElasticBalls(){
 		document.body.append(this.elem);
 	}
 	
+	
+	/**
+	 * 
+	 */
 	function vec(X, Y) {
 		this.X = X;
 		this.Y = Y;
 	}
 	
+	
+	/**
+	 * 
+	 */
 	function springForce(i, j, spring)
 	{
-	    var dx = (dots[i].X - dots[j].X);
-	    var dy = (dots[i].Y - dots[j].Y);
-	    var len = Math.sqrt(dx*dx + dy*dy);
-	    if (len > SEGLEN) {
-	        var springF = SPRINGK * (len - SEGLEN);
-	        spring.X += (dx / len) * springF;
-	        spring.Y += (dy / len) * springF;
-	    }
+		var dx = (dots[i].X - dots[j].X);
+		var dy = (dots[i].Y - dots[j].Y);
+		var len = Math.sqrt(dx*dx + dy*dy);
+		if (len > SEGLEN) {
+			var springF = SPRINGK * (len - SEGLEN);
+			spring.X += (dx / len) * springF;
+			spring.Y += (dy / len) * springF;
+		}
 	}
 	
-	function animate() {
-		for (let i = 0 ; i < nDots; i++ ) {
-			
-			if (followmouse && !i) {
-				dots[0].X = Xpos;
-				dots[0].Y = Ypos;
-				dots[0].obj.display = 'none';
-				continue;
-			}
-			
-		    var spring = new vec(0, 0);
-		    if (i > 0) {
-		        springForce(i-1, i, spring);
-		    }
-		    if (i < (nDots - 1)) {
-		        springForce(i+1, i, spring);
-		    }
-		    
-		     var resist = new vec(-dots[i].dx * RESISTANCE,
-		        -dots[i].dy * RESISTANCE);
-		    
-		     var accel = new vec((spring.X + resist.X)/ MASS,
-		        (spring.Y + resist.Y)/ MASS + GRAVITY);
-		    
-		      dots[i].dx += (DELTAT * accel.X);
-		    dots[i].dy += (DELTAT * accel.Y);
-		    
-		      if (Math.abs(dots[i].dx) < STOPVEL &&
-		        Math.abs(dots[i].dy) < STOPVEL &&
-		        Math.abs(accel.X) < STOPACC &&
-		        Math.abs(accel.Y) < STOPACC) {
-		        dots[i].dx = 0;
-		        dots[i].dy = 0;
-		    }
-		    
-		     dots[i].X += dots[i].dx;
-		    dots[i].Y += dots[i].dy;
-		
-			var height, width;
-			height = window.innerHeight + document.scrollTop;
-			width = window.innerWidth + document.scrollLeft;
 	
-		    if (dots[i].Y >=  height - DOTSIZE - 1) {
-		        if (dots[i].dy > 0) {
-		            dots[i].dy = BOUNCE * -dots[i].dy;
-		        }
-		        dots[i].Y = height - DOTSIZE - 1;
-		    }
-		    if (dots[i].X >= width - DOTSIZE) {
-		        if (dots[i].dx > 0) {
-		            dots[i].dx = BOUNCE * -dots[i].dx;
-		        }
-		        dots[i].X = width - DOTSIZE - 1;
-		    }
-		    if (dots[i].X < 0) {
-		        if (dots[i].dx < 0) {
-		            dots[i].dx = BOUNCE * -dots[i].dx;
-		        }
-		        dots[i].X = 0;
-		    }
-		    
-		    dots[i].obj.left = dots[i].X + "px";
-		    dots[i].obj.top =  dots[i].Y + "px";
+	/**
+	 * Animates the specified dot
+	 */
+	function animate(dot,i) {
+		// originally animate did not take a parameter
+		// if no parameter was passed, assume the global array of dots
+		if(!dot){
+			animate(dots);
+			return;
 		}
+		// originally animate worked on an array of dots. If they passed an 
+		// array, animate each individual dot.
+		if(Array.isArray(dot)){
+			dot.forEach(animate);
+			return;
+		}
+		
+		if (followmouse && !i) {
+			dot.X = Xpos;
+			dot.Y = Ypos;
+			dot.obj.display = 'none';
+			return;
+		}
+		
+		var spring = new vec(0, 0);
+		if (i > 0) {
+			springForce(i-1, i, spring);
+		}
+		if (i < (nDots - 1)) {
+			springForce(i+1, i, spring);
+		}
+		
+		var resist = new vec(-dot.dx * RESISTANCE, -dot.dy * RESISTANCE);
+		
+		var accel = new vec((spring.X + resist.X)/ MASS, (spring.Y + resist.Y)/ MASS + GRAVITY);
+		
+		dot.dx += (DELTAT * accel.X);
+		dot.dy += (DELTAT * accel.Y);
+		
+		if (Math.abs(dot.dx) < STOPVEL && Math.abs(dot.dy) < STOPVEL && Math.abs(accel.X) < STOPACC && Math.abs(accel.Y) < STOPACC) {
+			dot.dx = 0;
+			dot.dy = 0;
+		}
+		
+		dot.X += dot.dx;
+		dot.Y += dot.dy;
+		
+		let height = window.innerHeight; // - win.scrollTop;
+		let width = window.innerWidth; // - document.scrollLeft;
+		
+		if (dot.Y >=  height - dot.elem.offsetHeight - 1) {
+			if (dot.dy > 0) {
+				dot.dy = BOUNCE * -dot.dy;
+			}
+			dot.Y = height - dot.elem.offsetHeight - 1;
+		}
+		if (dot.Y < 0) {
+			if (dot.dy < 0) {
+				dot.dy = BOUNCE * -dot.dy;
+			}
+			dot.Y = 0;
+		}
+		
+		if (dot.X >= width - dot.elem.offsetWidth) {
+			if (dot.dx > 0) {
+				dot.dx = BOUNCE * -dot.dx;
+			}
+			dot.X = width - dot.elem.offsetWidth - 1;
+		}
+		if (dot.X < 0) {
+			if (dot.dx < 0) {
+				dot.dx = BOUNCE * -dot.dx;
+			}
+			dot.X = 0;
+		}
+		
+		dot.obj.left = dot.X + "px";
+		dot.obj.top =  dot.Y + "px";
 	}
 	
 }
